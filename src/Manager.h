54f28637f2f2c9fb3a6ddfc0179192f65c825851
Rildo05 <amarildo.pashaj@fti.edu.al>
2022-12-03 07:03:51 -0500

    Preeleminary setup and tests

--- src/Manager.h
+++ src/Manager.h
@@ -8,7 +8,27 @@
 #include "ManagerInterface.h"
 
 namespace ClassProject {
+//using namespace ClassProject;
+    class Manager : public ManagerInterface {
+    public:
+        struct Node{
+            BDD_ID node;
+            BDD_ID node_high;
+            BDD_ID node_low;
+            BDD_ID top_var;
+            std::string label;
+        };
+        BDD_ID createVar(const std::string &label);
+        const BDD_ID &True();
+        const BDD_ID &False();
 
+        Manager(){//Ctor
+            //Node first_node = {0,0,0,0,"label"};
+        }
+        ~Manager(){//Dtor
+
+        }
+    };
 }
 
 #endif
