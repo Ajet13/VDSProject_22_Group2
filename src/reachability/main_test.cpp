7068b6287eda0fd4901a62f413f230a5c8a62023
kristilleshaj <kristilleshaj@gmail.com>
2023-01-16 15:07:01 +0100

    check of a 4 variable state machine that goes at states 0,1,2,3,12,13,14,15

--- src/reachability/main_test.cpp
+++ src/reachability/main_test.cpp
@@ -20,7 +20,8 @@ TEST (function, iscorrect) {
     std::vector<BDD_ID> stateVars = fsm->getStates();
     BDD_ID first = fsm->or2(stateVars[0], 1);
     BDD_ID second = fsm->or2(stateVars[1], 1);           //check that the first time Identity function is present
-    EXPECT_THROW(fsm->setTransitionFunctions({first,second,0}),std::runtime_error);
+    EXPECT_THROW(fsm->setTransitionFunctions({first, second, 0}), std::runtime_error);     //to many arguments
+    EXPECT_THROW(fsm->setTransitionFunctions({first, 256}), std::runtime_error);         //unknown function ID
     EXPECT_TRUE(fsm->isReachable({false, false}));  //check transition function initialised correctly
     EXPECT_FALSE(fsm->isReachable({false, true}));
     EXPECT_FALSE(fsm->isReachable({true, false}));
@@ -34,7 +35,7 @@ TEST (function, iscorrect) {
 
 TEST (initstates, iscorrect) {
     std::unique_ptr<ClassProject::Reachability> fsm = std::make_unique<ClassProject::Reachability>(2);
-    EXPECT_THROW(fsm->setInitState({true, true, true,true}),std::runtime_error);
+    EXPECT_THROW(fsm->setInitState({true, true, true, true}), std::runtime_error);
     EXPECT_TRUE(fsm->isReachable({false, false}));  //check init state initialised correctly
     EXPECT_FALSE(fsm->isReachable({false, true}));
     EXPECT_FALSE(fsm->isReachable({true, false}));
@@ -44,6 +45,36 @@ TEST (initstates, iscorrect) {
     EXPECT_FALSE(fsm->isReachable({false, true}));
     EXPECT_FALSE(fsm->isReachable({true, false}));
     EXPECT_TRUE(fsm->isReachable({true, true}));
+
+}
+
+TEST (fsmtest, four_vars) {
+    std::unique_ptr<ClassProject::Reachability> fsm4 = std::make_unique<ClassProject::Reachability>(4);
+    std::vector<BDD_ID> stateVars2 = fsm4->getStates();
+    std::vector<BDD_ID> transitionFunctions;
+    BDD_ID s0 = stateVars2.at(0);
+    BDD_ID s1 = stateVars2.at(1);
+    BDD_ID s2 = stateVars2.at(2);
+    BDD_ID s3 = stateVars2.at(3);
+
+    transitionFunctions.push_back(fsm4->neg(s0));
+    transitionFunctions.push_back(fsm4->xor2(s1, s0));
+    transitionFunctions.push_back(
+            fsm4->or2(fsm4->and2(s2, fsm4->nand2(s1, s0)), fsm4->and2(fsm4->and2(s1, s0), fsm4->neg(s3))));
+    transitionFunctions.push_back(
+            fsm4->or2(fsm4->and2(s2, fsm4->nand2(s1, s0)), fsm4->and2(fsm4->and2(s1, s0), fsm4->neg(s3))));
+
+    fsm4->setTransitionFunctions(transitionFunctions);
+
+    fsm4->setInitState({false, false, false, false});
+    for (unsigned int i = 0; i < 16; i++) {
+        std::vector<bool> state_check = {(i >> 0) & 1 ? true : false, (i >> 1) & 1 ? true : false,
+                                         (i >> 2) & 1 ? true : false, (i >> 3) & 1 ? true : false};
+        if (i > 3 && i < 12)
+            ASSERT_FALSE(fsm4->isReachable(state_check));
+        else
+            ASSERT_TRUE(fsm4->isReachable(state_check));
+    }
 
 }
 
